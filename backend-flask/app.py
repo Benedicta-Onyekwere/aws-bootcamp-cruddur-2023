c8d0b16ba18eecafee2eca9740cf8f6c1bcdfd26
Benedicta Onyekwere <onyekwerebenedicta@gmail.com>
2023-03-16 10:29:33 +0000

    added healthcheck

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -5,6 +5,7 @@ import os
 
 from services.home_activities import *
 from services.notifications_activities import *
+from services.healthcheck_activities import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -69,6 +70,11 @@ def data_home():
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_notifications():
   data = NotificationsActivities.run()
+  return data, 200
+
+@app.route("/api/activities/healthcheck", methods=['GET'])
+def data_healthcheck():
+  data = HealthActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
