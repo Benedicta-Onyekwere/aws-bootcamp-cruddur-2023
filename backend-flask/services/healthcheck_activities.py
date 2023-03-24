c1b4fb3ff8ac6987b0399674e477d8355062b398
Benedicta Onyekwere <onyekwerebenedicta@gmail.com>
2023-03-24 07:17:40 +0000

    updated

--- backend-flask/services/healthcheck_activities.py
+++ backend-flask/services/healthcheck_activities.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-class NotificationsActivities:
+class HealthcheckActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     results = [{
