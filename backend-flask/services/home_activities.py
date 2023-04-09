58b227980309de92663202297f71817f2104335b
Techgirl-BO Florence <benniequeen@yahoo.com>
2023-04-09 09:39:46 +0000

    post confirmation lambda

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -8,9 +8,9 @@ class HomeActivities:
   def run(cognito_user_id=None):
     print("HOME ACTIVITY")
     #logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activites-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
+     with tracer.start_as_current_span("home-activites-mock-data"):
+       span = trace.get_current_span()
+       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
 
       sql = query_wrap_array("""
