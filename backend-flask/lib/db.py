78547ad38b8422eebc43e02c41c3d5d3b320b3c1
Benedicta Onyekwere <onyekwerebenedicta@gmail.com>
2023-04-25 00:45:06 +0000

    implement dynamodb into application

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -88,7 +88,11 @@ class Db:
       with conn.cursor() as cur:
         cur.execute(sql,params)
         json = cur.fetchone()
+        #if json is not None:
         return json[0]
+        #else:
+         #   return None
+        #return json[0]
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
