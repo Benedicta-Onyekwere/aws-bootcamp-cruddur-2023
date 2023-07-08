dbb0b1d7f059576d8830bcf58f5063816c914d24
Benedicta Onyekwere <onyekwerebenedicta@gmail.com>
2023-07-08 02:02:27 +0100

    render avatar

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -30,13 +30,16 @@ def handler(event:, context:)
   else
     token = event['headers']['authorization'].split(' ')[1]
     puts({ step: 'presigned url', access_token: token }.to_json)
-    
+
     body_hash = JSON.parse(event["body"])
     extension = body_hash["extension"]
 
     decoded_token = JWT.decode token, nil, false
-    puts decoded_token
     cognito_user_uuid = decoded_token[0]['sub']
+    #cognito_user_uuid = event["requestContext"]["authorizer"]["lambda"]["sub"]
+
+    puts({step:'presign url', sub_value: cognito_user_uuid}.to_json)
+
     s3 = Aws::S3::Resource.new
     bucket_name = ENV["UPLOADS_BUCKET_NAME"]
     object_key = "#{cognito_user_uuid}.#{extension}"
@@ -44,20 +47,21 @@ def handler(event:, context:)
     puts({object_key: object_key}.to_json)
 
     obj = s3.bucket(bucket_name).object(object_key)
-    url = obj.presigned_url(:put, expires_in: 300)
+    url = obj.presigned_url(:put, expires_in: 60 * 5)
     url # this is the data that will be returned
-    body = { url: url }.to_json
-    {
+    
+    body = {url: url}.to_json
+    { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
         "Access-Control-Allow-Origin": frontend_url,
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
-      statusCode: 200,
-      body: body
+      statusCode: 200, 
+      body: body 
     }
-  end
-end
+  end # if 
+end # def handler
 
      
 
